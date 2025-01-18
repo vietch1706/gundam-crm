6d35f2799bfc6fc56a64d5c0d6f65f5bbfc811a6
Quang <quangtm1209@gmail.com>
2025-01-18 14:49:55 +0700

    init project

---
+++ finn.sh
@@ -0,0 +1,41 @@
+echo "Enter cmd"
+read cmd
+if [ $cmd == "clear" ];
+then
+    php artisan view:clear
+    php artisan cache:clear
+    php artisan route:clear
+    echo "Clear all"
+elif [ $cmd == "docker" ];
+then
+    echo "Go to docker bash"
+    docker exec -it php_gundam bash
+elif [ $cmd == "linux" ];
+then
+sudo chown -R $USER:www-data plugins/
+chmod -R 775 plugins/
+sudo chown -R $USER:www-data storage/
+chmod -R 775 storage/
+echo "Permisstion OK!!"
+elif [ $cmd == "v1" ];
+then
+    php artisan swagger:generate openapi/v1.yml
+    echo "Generate file v1"
+elif [ $cmd == "install" ];
+then
+    composer i
+    cp .env.example .env
+    php artisan key:generate
+    mysql -u root -padmin
+    echo "Install"
+elif [ $cmd == "plugin" ]; then
+    echo "Enter plugin name"
+    read plugin_name
+    php artisan plugin:refresh skechers.$plugin_name
+    echo "Refreshed plugin skechers.$plugin_name"
+elif [ $cmd == "migrate" ]; then
+    php artisan october:migrate
+    echo "Migrate done"
+else
+    echo "Sorry, try for the next time"
+fi
