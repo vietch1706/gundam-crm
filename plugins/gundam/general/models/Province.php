b2067982b4f08ee3e30cc7f80eaf2035022c16bb
Finn <quangtm1209>
2025-02-12 20:17:06 +0700

    general(area): cms

---
+++ plugins/gundam/general/models/Province.php
@@ -0,0 +1,27 @@
+<?php namespace Gundam\General\Models;
+
+use Model;
+
+/**
+ * Model
+ */
+class Province extends Model
+{
+    use \October\Rain\Database\Traits\Validation;
+
+
+    /**
+     * @var string table in the database used by the model.
+     */
+    public $table = 'gundam_general_province';
+
+    /**
+     * @var array rules for validation.
+     */
+    public $rules = [
+    ];
+
+    public $hasMany = [
+      'districts' => [District::class, 'key' => 'province_id'],
+    ];
+}
